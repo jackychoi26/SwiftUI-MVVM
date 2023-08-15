415bfa169eda5f3129edb317247de732091d2094
jackychoi26 <gmu1233@gmail.com>
2023-08-15 21:05:33 +0800

    Initial Commit

---
+++ SwiftUI-MVVM/SwiftUI_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVVMApp.swift
+//  SwiftUI-MVVM
+//
+//  Created by Choi Kin Lung on 15/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
