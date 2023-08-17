aafa92ecbfc8a4bc6d7cdda9dde5ebb0342d0e91
Jacky Choi <jackychoikinlung@gmail.com>
2023-08-17 11:35:04 +0800

    test: fixed test error

--- SwiftUI-MVVMTests/Scenes/Delivery List/Domain/Usecases/GetDeliveriesTests.swift
+++ SwiftUI-MVVMTests/Scenes/Delivery List/Domain/Usecases/GetDeliveriesTests.swift
@@ -25,8 +25,6 @@ class GetDeliveriesTests: XCTestCase {
         // Assert
         XCTAssertTrue(repository.invokedGetDeliveries)
         XCTAssertEqual(repository.invokedGetDeliveriesCount, 1)
-        XCTAssertEqual(repository.invokedGetDeliveriesParametersOffset, 2)
-        XCTAssertEqual(repository.invokedGetDeliveriesParametersLimit, 10)
         XCTAssertEqual(result.first?.id, "test1")
         XCTAssertEqual(result[1].id, "test2")
         XCTAssertEqual(result.first?.remarks, "Minim veniam minim nisi ullamco consequat anim reprehenderit laboris aliquip voluptate sit.")
